235d52e86124c613fe134820d9f4c35c05ef4146
ncg <tyu4778@gmail.com>
2022-05-11 23:00:19 +0900

    React 2.쇼핑몰 프로젝트 3. import, export 사용하기 - 상품목록 컴포넌트로 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import {useState} from "react";
 import './App.css';
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import img from './img/shoe.png';
+import data from './data.js';
 
 function App() {
+    let [modal, setModal] = useState(false);
+
+    let [shoes] = useState(data);
+
   return (
     <div className="App">
         <Navbar bg="dark" variant="dark" className={""}>
@@ -25,29 +31,50 @@ function App() {
 
         <div className="container">
             <div className="row align-items-center">
-                <div className="col">
-                    {/* public 폴더 이미지 쓰는 권장방식 */}
-                    {/*<img src={process.env.PUBLIC_URL + '/logo192.png'} style={{width : "80%"}}/>*/}
+                {
+                    shoes.map(function(data, i) {
+                        return (
+                            <Modal shoe={data} i={i}></Modal>
+                        )
+                    })
+                }
+                {/*<div className="col">
+                     public 폴더 이미지 쓰는 권장방식
+                    <img src={process.env.PUBLIC_URL + '/logo192.png'} style={{width : "80%"}}/>
 
                     <img src={"https://codingapple1.github.io/shop/shoes1.jpg"} style={{width : "80%"}}/>
-                    <h4>상품명</h4>
-                    <p>상품설명</p>
+                    <h4>{shoes[0].title}</h4>
+                    <p>{shoes[0].content}</p>
+                    <p>{shoes[0].price}</p>
                 </div>
                 <div className="col">
                     <img src={"https://codingapple1.github.io/shop/shoes2.jpg"} style={{width : "80%"}}/>
-                    <h4>상품명</h4>
-                    <p>상품설명</p>
+                    <h4>{shoes[1].title}</h4>
+                    <p>{shoes[1].content}</p>
+                    <p>{shoes[1].price}</p>
                 </div>
                 <div className="col">
                     <img src={"https://codingapple1.github.io/shop/shoes3.jpg"} style={{width : "80%"}}/>
-                    <h4>상품명</h4>
-                    <p>상품설명</p>
-                </div>
+                    <h4>{shoes[2].title}</h4>
+                    <p>{shoes[2].content}</p>
+                    <p>{shoes[2].price}</p>
+                </div>*/}
             </div>
         </div>
 
     </div>
   );
+}
+
+const Modal = (props) => {
+    return (
+        <div className="col">
+            <img src={"https://codingapple1.github.io/shop/shoes" + (props.i+1) + ".jpg"} style={{width : "80%"}}/>
+            <h4>{props.shoe.title}</h4>
+            <p>{props.shoe.content}</p>
+            <p>{props.shoe.price}</p>
+        </div>
+    );
 }
 
 export default App;
